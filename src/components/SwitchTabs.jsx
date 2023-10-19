1781f973fca6bfee41d9132703819af6e4d01acb
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-10-19 20:56:52 +0330

    fixed bug SwitchTabs component

--- src/components/SwitchTabs.jsx
+++ src/components/SwitchTabs.jsx
@@ -4,13 +4,7 @@ import { useEffect, useState } from "react"
 function SwitchTabs({ values, switchTabsHandler }) {
 
     const [type, setType] = useState(values[0])
-    const [left, setLeft] = useState(false)
-
-    const activeTab = (val) => {
-        setType(val)
-        setLeft(prev => !prev)
-    }
-
+    
     useEffect(() => {
         switchTabsHandler(type)
     }, [type])
@@ -19,14 +13,13 @@ function SwitchTabs({ values, switchTabsHandler }) {
         <div className='relative flex items-center gap-1.5 p-0.5 bg-white text-black rounded-3xl h-9 shadow-lg shadow-black'>
             {values.map((val, index) => (
                 <div
-                    onClick={() => activeTab(val)}
+                    onClick={() => setType(val)}
                     key={index}
-                    className={`${type === val ? ' text-white' : 'text-black'} capitalize cursor-pointer flex items-center justify-center h-full w-28 font-semibold rounded-3xl z-20`}
+                    className={`${type === val ? ' text-white gradient-1' : 'text-black bg-white'} transition-colors duration-300 capitalize cursor-pointer flex items-center justify-center h-full w-28 font-semibold rounded-3xl z-20`}
                 >
                     {val}
                 </div>
             ))}
-            <span className={`gradient-1 w-28 h-[90%] absolute left-[${left ? 51 : 1}%] rounded-3xl z-10 transition-all duration-400`}></span>
         </div>
     )
 }
