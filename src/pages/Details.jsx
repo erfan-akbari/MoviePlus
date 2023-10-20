5ed9d464e55a16e51e9addf0f318b84b8f1f7896
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-10-20 21:13:24 +0330

    Development Details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useParams } from "react-router-dom"
+import useFetch from "../hooks/useFetch"
+import DetailsBanner from "../components/DetailsBanner"
 
 function Details() {
+  const { mediaType, id } = useParams()
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`)
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`)
+
   return (
-    <div>Details</div>
+    <main>
+      <DetailsBanner videos={data?.result?.[0]} crew={credits?.crew} />
+    </main>
   )
 }
 
