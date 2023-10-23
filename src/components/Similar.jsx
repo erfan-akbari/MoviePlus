a7c786114164d6db25b8280826da75e7f74e3008
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-10-23 20:24:17 +0330

    Development Similar Section

---
+++ src/components/Similar.jsx
@@ -0,0 +1,25 @@
+import Carousel from "./Carousel"
+import useFetch from "../hooks/useFetch"
+import ContentWrapper from "./ContentWrapper"
+
+function Similar({ mediaType, id }) {
+
+    const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`)
+
+    const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies"
+    console.log(data);
+    return (
+        <section className="my-10">
+            {data?.results && (
+                <Carousel
+                    title={title}
+                    data={data?.results}
+                    loading={loading}
+                    type={mediaType}
+                />
+            )}
+        </section>
+    )
+}
+
+export default Similar
